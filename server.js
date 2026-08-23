5ec1d0e6148f4beb00e6ecd7e697e1411876b815
Arihant-rgb <arihantpatil7676@gmail.com>
2026-08-23 16:31:15 +0530

    add delete button for each job

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express= require("express");
 const fs= require('fs');
+
 const app= express();
 const PORT=3000;
 app.set("view engine","ejs");
@@ -29,6 +30,19 @@ app.post('/jobs',(req,res)=>{
 
     res.redirect('/');
 });
+
+app.post("/jobs/:id/delete",(req,res)=>{
+
+    let data= JSON.parse(fs.readFileSync('./data.json','utf-8'));
+
+    const jobId= Number(req.params.id);
+
+    data.jobs = data.jobs.filter(job => job.id != jobId);
+
+    fs.writeFileSync('./data.json',JSON.stringify(data,null,2));
+
+    res.redirect('/');
+})
 
 app.listen(PORT, ()=>{
     console.log(`running server on https://localhost:${PORT}`);
